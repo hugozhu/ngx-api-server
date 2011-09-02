ee55bc71e5ff54caa96b8e696b4697e4d2c53c15
Jon Yao <corejava@126.com>
2011-09-02 23:01:53 +0800

    Test on Win7 by ssh rsa key

--- adx.sh
+++ adx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#edit by Jon on Win7 by git on rsa key
 echo "Clean log files!"
 rm tmp/logs/*.log
+
 echo "Restart Nginx!"
 bin/restart_nginx.sh
+
 ps -ef |grep nginx|grep worker
+
 echo "Nginx is now started!"
