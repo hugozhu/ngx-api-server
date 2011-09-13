4e723ff79b16579cf07da84557debbd2dca13b4e
hugozhu <hugozhu@gmail.com>
2011-09-13 23:32:21 +0800

    added makefile

---
+++ bin/code_gen.lua
@@ -0,0 +1,8 @@
+---
+-- Created by IntelliJ IDEA.
+-- User: hugozhu
+-- Date: 9/13/11
+-- Time: 11:01 PM
+-- To change this template use File | Settings | File Templates.
+--
+
---
+++ lib/Template.lua
@@ -0,0 +1,8 @@
+---
+-- Created by IntelliJ IDEA.
+-- User: hugozhu
+-- Date: 9/13/11
+-- Time: 5:37 PM
+-- To change this template use File | Settings | File Templates.
+--
+
